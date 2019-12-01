e70df4cf64c48df57b7c6a8effac993f1f4d938a
amonrs <amonsthapit@live.com>
2019-11-30 22:13:16 -0800

    usydfhsl

--- convexhull.py
+++ convexhull.py
@@ -1,11 +1,21 @@
 # Convex Hull algorithm
 # Amon , Pragalva
+import random
 
-def convexhull():
+def convexhull_jarvis_algo(points):
+    # https://iq.opengenus.org/gift-wrap-jarvis-march-algorithm-convex-hull/
     pass
+
+def convexhull_graham_scan(points):
+    pass
+
 
 
 def main():
-    pass
+    points = []
+    for i in range(20):
+        points.append((random.randint(0,20),random.randint(0,20)))
+    convexhull_jarvis_algo(points)
+    convexhull_graham_scan(points)
 if __name__ == '__main__':
     main()
